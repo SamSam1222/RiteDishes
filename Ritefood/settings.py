eaecae8c0785f8cc13685eb125fb74085768c5a6
SamSam1222 <samuelemenike4321@gmail.com>
2023-04-21 16:24:05 +0100

    templates

--- Ritefood/settings.py
+++ Ritefood/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'Ritefood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'W3OXZteVqURGIgAV15sf',
+        'HOST': 'containers-us-west-7.railway.app',
+        'PORT': '6020',
     }
 }
 
